d8e55b7df5bf354be42f925d0cd51433b1356c42
Dale Hennessey <drhennessey@gmail.com>
2025-04-06 14:27:16 -0400

    initial commit

---
+++ WebApplication1/WebApplication1/Data/Post.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+namespace WebApplication1.Data
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        [MaxLength(200)]
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        //public Blog Blog { get; set; }
+    }
+}
